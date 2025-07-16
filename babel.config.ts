9719f6e42a031c163ed3c7e482435a83dcebe664
Pranav-sai02 <pranavsaikasinadhuni2@gmail.com>
2025-07-16 07:08:10 +0530

    initial commit

---
+++ babel.config.ts
@@ -0,0 +1,16 @@
+module.exports = function (api: { cache: (arg0: boolean) => void; }) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            '@': './',
+          },
+        },
+      ],
+    ],
+  };
+};
